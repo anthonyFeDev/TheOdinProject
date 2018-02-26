7387a9f54ec7497983e0195ad78ff143469b1774
Anthony Ruffin <aruffincs@gmail.com>
2018-02-25 22:53:24 -0500

    added display library function and set it to display on page load

--- JS - Library/library.js
+++ JS - Library/library.js
@@ -6,19 +6,24 @@ let myLibrary = [
 ];
 
 // Display books in a list
-myLibrary.forEach((book) => {
-    let li = document.createElement("li");
-    let text = document.createTextNode(book);
-    li.appendChild(text);
-    document.getElementById("book-list").appendChild(li);
-});
+const displayList = () => {
+    myLibrary.forEach((book) => {
+        let li = document.createElement("li");
+        let text = document.createTextNode(book);
+        li.appendChild(text);
+        document.getElementById("book-list").appendChild(li);
+    });
+
+}
 
 // Add new book to list
 const addBookToLibrary = () => {
+    let x =[];
     let newBook = document.getElementById("new-book").value;
 
     //Push new book to array
     myLibrary.push(newBook);
     console.log("book added");
     console.log(myLibrary);
+    document.getElementById("book-list").appendChild(li);
 };
