05bd1d6fe69f5d8011142018d8a8485b4ca9ab6c
Anthony Ruffin <aruffincs@gmail.com>
2018-02-25 21:15:48 -0500

    added library array in index.js

--- JS - Library/library.js
+++ JS - Library/library.js
@@ -1,9 +1,14 @@
-let myLibrary = [];
+let myLibrary = [
+    "Holes",
+    "Of Mice and Men",
+    "The Bible",
+    "Romeo and Juliet"
+];
 
-const book = () => {
-
-}
-
-const addBookToLibrary = () => {
-
-}
+// Display books in a list
+myLibrary.forEach((book) => {
+    let li = document.createElement("li");
+    let text = document.createTextNode(book);
+    li.appendChild(text);
+    document.getElementById("book-list").appendChild(li);
+});
