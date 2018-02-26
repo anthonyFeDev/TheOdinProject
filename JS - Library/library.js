e62590092c25161a9d6184d4de2d237374e611cf
Anthony Ruffin <aruffincs@gmail.com>
2018-02-25 23:06:27 -0500

    added a note to self

--- JS - Library/library.js
+++ JS - Library/library.js
@@ -19,11 +19,12 @@ const displayLibrary = () => {
 // Add new book to list
 const addBookToLibrary = () => {
     let newBook = document.getElementById("new-book").value;
-
     //Push new book to array
     myLibrary.push(newBook);
-    console.log("book added");
-    console.log(myLibrary);
+    // console.log("book added");
+    // console.log(myLibrary);
     displayLibrary(newBook);
     // document.getElementById("book-list").appendChild(li);
+
+    //note to self: entire array re-renders
 };
