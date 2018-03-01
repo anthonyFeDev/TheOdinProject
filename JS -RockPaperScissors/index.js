0b573459f04e94e71d281e3370083797fb3d4eba
Anthony Ruffin <aruffincs@gmail.com>
2018-02-28 22:54:27 -0500

    added getPlayerSelection and getComputerSelection functions

--- JS -RockPaperScissors/index.js
+++ JS -RockPaperScissors/index.js
@@ -5,14 +5,21 @@ const gameOptions = [
     "Scissors"
 ];
 
-const playRound = () => {
+const getPlayerSelection = () => {
     // Get option player selected
     let playerSelection = document.getElementById("player-selection").value;
     console.log(playerSelection);
+};
 
+const getComputerSelection = () => {
     // Computer selection - grab random item from gameOptions array
     let computerSelection = gameOptions[Math.floor(Math.random() * gameOptions.length)] 
     console.log(computerSelection);
+};
+
+const playRound = () => {
+    getPlayerSelection();
+    getComputerSelection();
 };
 
 const game = () => {
