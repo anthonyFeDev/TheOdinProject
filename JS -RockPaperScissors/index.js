2f859dcb1f7f61af8433310b470fab89d87637f9
Anthony Ruffin <aruffincs@gmail.com>
2018-02-28 22:38:01 -0500

    added computer selection that grabs random item from array

--- JS -RockPaperScissors/index.js
+++ JS -RockPaperScissors/index.js
@@ -6,8 +6,13 @@ const gameOptions = [
 ];
 
 const playRound = () => {
-    playerSelection = document.getElementById("player-selection").value;
+    // Get option player selected
+    let playerSelection = document.getElementById("player-selection").value;
     console.log(playerSelection);
+
+    // Computer selection - grab random item from gameOptions array
+    let computerSelection = gameOptions[Math.floor(Math.random() * gameOptions.length)] 
+    console.log(computerSelection);
 };
 
 const game = () => {
