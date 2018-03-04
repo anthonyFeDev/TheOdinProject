bda457951e0da2428abb3b0866b07f48114f37b5
Anthony Ruffin <aruffincs@gmail.com>
2018-03-03 19:56:30 -0500

    added a feature that shows what the computer chose

--- JS -RockPaperScissors/index.js
+++ JS -RockPaperScissors/index.js
@@ -3,17 +3,17 @@ const getPlayerSelection = () => {
     // Get option player selected
     let playerSelection = document.getElementById("player-selection").value;
     let showPlayerSelection = document.getElementById("selection").innerHTML = playerSelection;
-    
+
     return playerSelection;
 };
 
 const getComputerSelection = () => {
     //Create an array of options
     const gameOptions = ["Rock", "Paper", "Scissors"];
-
     // Computer selection - grab random item from gameOptions array
     let computerSelection = gameOptions[Math.floor(Math.random() * gameOptions.length)];
-
+    
+    let showComputerSelection = document.getElementById("comp-selection").innerHTML = computerSelection;
     return computerSelection;
 };
 
