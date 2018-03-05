2983ddda524ca76e7cb6c82bb0d023702739046f
Anthony Ruffin <aruffincs@gmail.com>
2018-03-04 20:53:46 -0500

    finished playRound() function...completed the functionality of game

--- JS -RockPaperScissors/index.js
+++ JS -RockPaperScissors/index.js
@@ -2,6 +2,7 @@
 const getPlayerSelection = () => {
     // Get option player selected
     let playerSelection = document.getElementById("player-selection").value;
+    playerSelection = playerSelection.toLowerCase();
     let showPlayerSelection = document.getElementById("selection").innerHTML = playerSelection;
 
     return playerSelection;
@@ -9,10 +10,10 @@ const getPlayerSelection = () => {
 
 const getComputerSelection = () => {
     //Create an array of options
-    const gameOptions = ["Rock", "Paper", "Scissors"];
+    const gameOptions = [ "paper", "rock", "scissors"];
+    
     // Computer selection - grab random item from gameOptions array
     let computerSelection = gameOptions[Math.floor(Math.random() * gameOptions.length)];
-    
     let showComputerSelection = document.getElementById("comp-selection").innerHTML = computerSelection;
     return computerSelection;
 };
@@ -20,6 +21,30 @@ const getComputerSelection = () => {
 const playRound = () => {
     getPlayerSelection();
     getComputerSelection();
-    //console.log(getComputerSelection(), getPlayerSelection());
+
+    let computerPick = getComputerSelection();
+    let playerPick = getPlayerSelection();
+    
+    let winner = "";
+
+    if (playerPick == 'rock' && computerPick == 'rock') {
+        winner = document.getElementById("winner").innerHTML = "it's a tie";
+    } else if (playerPick == 'rock' && computerPick == 'paper') {
+        winner = document.getElementById("winner").innerHTML = "You lose";
+    } else if (playerPick == 'rock' && computerPick == 'scissors') {
+        winner = document.getElementById("winner").innerHTML = "You won!";
+    } else if (playerPick == 'paper' && computerPick == 'scissors') {
+        winner = document.getElementById("winner").innerHTML = "You lose";
+    } else if (playerPick == 'paper' && computerPick == 'rock') {
+        winner = document.getElementById("winner").innerHTML = "You won!";
+    } else if (playerPick == 'paper' && computerPick == 'paper') {
+        winner = document.getElementById("winner").innerHTML = "It's a tie";
+    } else if (playerPick == 'scissors' && computerPick == 'paper') {
+        winner = document.getElementById("winner").innerHTML = "You won!";
+    } else if (playerPick == 'scissors' && computerPick == 'rock') {
+        winner =document.getElementById("winner").innerHTML = "You lose";
+    } else if (playerPick == 'scissors' && computerPick == 'scissors') {
+        winner = document.getElementById("winner").innerHTML = "It's a tie";
+    }
     
 };
