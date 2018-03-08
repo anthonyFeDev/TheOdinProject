2871ab955bb92a8a6d028b7a02e7c104eb8acd72
Anthony Ruffin <aruffincs@gmail.com>
2018-03-07 21:25:55 -0500

    attached className to div

--- JS - EtchAndSketch/index.js
+++ JS - EtchAndSketch/index.js
@@ -1,7 +1,14 @@
 
-let div = document.createElement("div");
-// let text = document.createTextNode("this is a div");
-// div.appendChild(text);
+//Playground code
+let numberOfDivs = 16;
 
-let element = document.getElementById("main");
-element.appendChild(div);
+for (let i = 0; i < numberOfDivs; i++) {
+    let div = document.createElement("div");
+    div.className = "grid-cell";
+    let text = document.createTextNode("this is a div");
+    div.appendChild(text);
+
+    let element = document.getElementById("main");
+    element.appendChild(div);
+}
+
