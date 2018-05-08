99645a8950d53ae04deb53e73c8eaad8a7774c1d
Anthony <aruffincs@gmail.com>
2018-05-08 10:20:30 -0400

    updates

---
+++ fetch-data/main.js
@@ -0,0 +1,21 @@
+// search for Gifs
+// select image and assign it to a variable 
+// const img = document.querySelector('img');
+
+// // add in fetch from the url
+// fetch('https://api.giphy.com/v1/gifs/translate?api_key=orbkuIOnZg8RqMbmOhWzDw5W5imRiUQN&s=vader', {mode: 'cors'})
+//     .then((response) => {
+//         return response.json();
+//     }).then((response) => {
+//         img.src = response.data.images.original.url;
+//     })
+
+
+// // Google Maps API
+// fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?query=123+main+street&key=AIzaSyCB-KYAYNte4UFhMdrKpDrULxzQ6L2Umr0', { mode: 'cors'})
+// .then((response) => {
+//     return response.json();
+// })
+// .then((response) => {
+//     console.log(response);
+// })
